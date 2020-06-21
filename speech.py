85f894431e97569ec7f6895b5f5cb93d93077f89
GamingDevilsCC <gamingdevilscc@gmail.com>
2020-06-21 14:49:57 -0400

    Add English to French translation

--- speech.py
+++ speech.py
@@ -1,12 +1,20 @@
 import speech_recognition as sr
+from googletrans import Translator
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say anything!")
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        print("Output:", text)
-    except:
-        print("I was not able to convert your speech.\nPlease try again.")
-        
+    while True:
+        print("Say anything!")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            if text == "quit":
+                print("Goodbye!")
+                break
+            trans = Translator()
+            t = trans.translate(text, src = 'en', dest = 'fr')
+            print("You said:", text)
+            print("In french:", t.text)
+
+        except:
+            print("I was not able to convert your speech.\nPlease try again.")
